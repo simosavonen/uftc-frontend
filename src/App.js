b8feb1064c936980458ecd72e0cc4fd9a8903d82
Salomon Honkala <salomon.honkala@gmail.com>
2019-06-26 13:24:30 +0300

    Testidataa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [challenges, setChallenges] = useState([]);
+
+  useEffect(() => {
+    console.log('effect');
+    axios.get('http://localhost:3001/challenges').then(response => {
+      console.log('promise fulfilled');
+      setChallenges(response.data);
+    });
+  }, []);
+
+  console.log('render', challenges.length, 'challenges');
+
   return (
-    <div className="App">
-
+    <div>
+      {challenges.map(challenge => (
+        <p>{challenge.name}</p>
+      ))}
     </div>
   );
-}
-
+};
 export default App;
