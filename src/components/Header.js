249bd2c05e22a764f346a377908cf270f16301b1
Simo Savonen <simo.savonen@gmail.com>
2019-06-28 01:06:43 +0300

    testailen bulma css kirjastoa, ambientian värejä ja logoa

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { ReactComponent as Logo } from '../logos/plank_UFTC.svg';
 
 const Header = props => {
-  return <div>UFTC - Logged in as {props.user.name}</div>;
+  return (
+    <nav className="navbar is-primary is-spaced">
+      <div className="container">
+        <div className="navbar-brand">
+          <div className="navbar-item">
+            <Logo width={105} height={95} />
+            Ultimate Functional Training Challenge
+          </div>
+        </div>
+        <div className="navbar-menu">
+          <div className="navbar-end">
+            <div className="navbar-item">Logged in as {props.user.name}</div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import './bulma-custom.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-
