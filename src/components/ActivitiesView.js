c6566cf94a945c7fc9b3df225aa303e7bf7b80fa
Simo Savonen <sipesa@utu.fi>
2019-07-04 10:55:32 +0300

    activitydetails lisätty, kajottu jonnekin muuallekin

--- src/components/ActivitiesView.js
+++ src/components/ActivitiesView.js
@@ -3,10 +3,13 @@ import ChallengeTitle from './ChallengeTitle';
 import WeeklyProgress from './WeeklyProgress';
 import RecentActivities from './RecentActivities';
 import ActivityMenu from './ActivityMenu';
+import ActivityDetails from './ActivityDetails';
+import axios from 'axios';
 
 const ActivitiesView = props => {
   const [activities, setActivities] = useState([]);
   const [challenge, setChallenge] = useState(null);
+  const [workouts, setWorkouts] = useState([]);
 
   useEffect(() => {
     if (props.challenges.length) {
@@ -15,11 +18,25 @@ const ActivitiesView = props => {
     }
   }, [props.challenges]);
 
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('loggedUser')).token;
+    axios.defaults.headers.common['Authorization'] = token;
+    axios
+      .get('http://localhost:3001/api/workouts')
+      .then(result => {
+        setWorkouts(result.data);
+        console.log(result.data);
+      })
+      .catch(error => console.log('weeklyprogress', error.message));
+  }, []);
+
   return (
     <div>
       <ChallengeTitle challenge={challenge} />
       <WeeklyProgress />
-      <RecentActivities activities={props.activities} />
+      <RecentActivities activities={props.activities} workouts={workouts} />
+
+      <ActivityDetails activities={activities} activitiesArrayIndex={0} />
 
       <ActivityMenu activities={activities} />
     </div>
---
+++ src/components/ActivityDetails.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ActivityDetails = activity => {
+  return <div>tänne ne yksiskohdat urheilulajista</div>;
+};
+
+export default ActivityDetails;
