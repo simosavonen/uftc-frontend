3442eee1192476ca2f4f92f603e55f0f3f3d7e5d
Salomon Honkala <salomon.honkala@gmail.com>
2019-07-05 07:26:01 +0300

    Add styling and pose animation to accordion

--- src/components/AccordionItem.js
+++ src/components/AccordionItem.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import posed from 'react-pose';
 
-const AccordionItem = props => {
-  console.log(props);
-
-  return <div>{props.name}</div>;
-};
+const AccordionItem = posed.div({
+  closed: { height: 0, opacity: 0 },
+  open: {
+    height: 'auto',
+    opacity: 100,
+    transition: {
+      ease: 'easeInOut',
+      duration: 150
+    }
+  }
+});
 
 export default AccordionItem;
--- src/components/AccordionList.js
+++ src/components/AccordionList.js
@@ -2,27 +2,24 @@ import React, { useState } from 'react';
 import AccordionItem from './AccordionItem';
 
 const AccordionList = props => {
-  const [opened, setClosed] = useState(false);
+  const [opened, setOpened] = useState(false);
 
   const handleClick = () => {
-    setClosed(!opened);
+    setOpened(!opened);
   };
+  //<div className="control is-fluid accordion-list__item" key={a.id}>
 
   return (
-    <div>
-      <button className="accordion-section" onClick={handleClick}>
+    <div className="container is-fluid">
+      <button className="button is-info" onClick={handleClick}>
         {props.type}
       </button>
-      <ul className="accordion-list">
-        {opened &&
-          props.activities.map(a => {
-            return (
-              <li className="accordion-list__item" key={a.id}>
-                <AccordionItem {...a} />
-              </li>
-            );
-          })}
-      </ul>
+
+      {props.activities.map(a => (
+        <AccordionItem pose={opened ? 'open' : 'closed'} key={a.id}>
+          <div>{a.name}</div>
+        </AccordionItem>
+      ))}
     </div>
   );
 };
