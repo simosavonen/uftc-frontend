00cb57561041b955c4ba1242545c9e0b9ceba7e3
Salomon Honkala <salomon.honkala@gmail.com>
2019-07-10 11:26:07 +0300

    Näytä haasteen aikarajat titlessä

--- src/components/ChallengeTitle.js
+++ src/components/ChallengeTitle.js
@@ -7,31 +7,39 @@ const placeholder = {
   name: 'loading...',
   pointsGoal: 7500,
   releaseDate: '2019-07-01',
-  startDate: '2019-07-01',
-  endDate: '2019-10-30',
+  startDate: '2019-07-10',
+  endDate: '2019-07-20',
   deadline: '2019-12-14',
   seriesTitle: 'Placeholder',
   pointBonus: 1,
   id: '5d1c5237c360412fbcc98dcc'
 };
 
-const ChallengeTitle = ({ challenge = placeholder }) => {
-  const aloitus = moment(challenge.startDate);
-  const lopetus = moment(challenge.endDate);
+const challengeTiming = challenge => {
+  const start = moment(challenge.startDate);
+  const end = moment(challenge.endDate);
 
-  const erotus = -1 * aloitus.diff(lopetus, 'days');
-
+  const length = end.diff(start, 'days');
   const today = moment();
+  let timing = '';
+  if (today < start) {
+    timing = 'Challenge starts ' + start.fromNow();
+  } else if (today.isSame(start, 'day')) {
+    timing = 'Challenge starts today!';
+  } else if (today.isAfter(start, 'day') && today.isSameOrBefore(end, 'day')) {
+    timing = 'Day ' + today.diff(start, 'days') + '/' + length;
+  } else if (today.isAfter(end, 'day')) {
+    timing = 'Challenge has ended!';
+  } else timing = '';
+  return timing;
+};
 
-  const aloituksesta = -1 * aloitus.diff(today, 'days');
-
+const ChallengeTitle = ({ challenge = placeholder }) => {
   return (
     <div>
       {challenge.name}
 
-      <p>
-        {aloituksesta} / {erotus}
-      </p>
+      <p>{challengeTiming(challenge)}</p>
     </div>
   );
 };
