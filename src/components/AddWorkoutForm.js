42d450ad5ffa08d6c3c3ba8d3391e619f80763b9
Simo Savonen <simo.savonen@gmail.com>
2019-07-12 20:16:48 +0300

    liikaa console.log spämmiä

--- src/components/AddWorkoutForm.js
+++ src/components/AddWorkoutForm.js
@@ -1,10 +1,6 @@
 import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 
-// propsina se funktio joka lisää suorituksen tietokantaan
-// funktio on määritelty tiedostossa App.js
-// lisäksi tieto urheilulajista
-
 const AddWorkoutForm = props => {
   const [amount, setAmount] = useState(1);
   const today = new Date().toISOString().substring(0, 10);
@@ -47,16 +43,7 @@ const AddWorkoutForm = props => {
       activity: props.activity.id
     };
     props.addWorkout(workout);
-
-    // luo olio workout, joka pitää sisällään tiedon
-    // lukumäärästä, päivästä, urheilulajin id.
-
-    // addWorkout lisää olioon vielä maininnan haasteen id:stä
   };
-
-  //return <form onSubmit={submit}>"lisää urheilusuoritus" -lomake</form>;
-
-  console.log('challenge', props.challenge);
 
   return (
     <div>
