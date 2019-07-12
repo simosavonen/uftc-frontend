1460df9d3dd934cff567fc23e418cf957223aca6
Simo Savonen <sipesa@utu.fi>
2019-07-12 09:16:07 +0300

    Now recent activies show more than 3 items if do it on same daylayout change in workoutview

--- src/components/WorkoutView.js
+++ src/components/WorkoutView.js
@@ -14,10 +14,20 @@ const placeholder = {
 const WorkoutView = ({ activity = placeholder, addWorkout, challenge }) => {
   return (
     <>
-      <div className="section columns is-centered">
+      <div className="section columns is-centered is-6">
         {/* tähän yleistä tietoa  urheilulajista */}
         <div className="column is-6">
           <h4 className="title is-4">{activity.name}</h4>
+        </div>
+      </div>
+      <section className="section columns is-centered">
+        <div className="column is-6 ">
+          <AddWorkoutForm addWorkout={addWorkout} activity={activity} challenge={challenge} />
+        </div>
+      </section>
+
+      <div className="section columns is-centered">
+        <div className="column is-6">
           <ul>
             <li>Sport activity type: {activity.type}</li>
             <li>Unit: {activity.unit}</li>
@@ -33,11 +43,6 @@ const WorkoutView = ({ activity = placeholder, addWorkout, challenge }) => {
           </ul>
         </div>
       </div>
-      <section className="section columns is-centered">
-        <div className="column is-6 ">
-          <AddWorkoutForm addWorkout={addWorkout} activity={activity} challenge={challenge} />
-        </div>
-      </section>
     </>
   );
 };
