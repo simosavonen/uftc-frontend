b079cd28df6382186273166f1eb06609c78d45e6
Simo Savonen <simo.savonen@gmail.com>
2019-07-31 14:57:19 +0300

    tyhjennä kanta vain kerran, testin aluksi, ei beforeEach, fixasin poiskommentoidun koodin, sen pitäisi toimia backendin kanssa nyt

--- cypress/integration/login_app_spec.js
+++ cypress/integration/login_app_spec.js
@@ -1,18 +1,14 @@
-import { isTSAnyKeyword } from '@babel/types';
-
 describe('Login app', function() {
-  beforeEach(function() {
-    /*cy.request({
-      method: 'POST',
-      url: 'http://localhost:4000/graphql',
-      body: { query: 'mutation {reset}' },
+  before(function() {
+    cy.request({
+      method: 'GET',
+      url: 'http://localhost:3001/api/reset',
       failsOnStatusCode: false
-    }).then(res => cy.log(res)); */
+    }).then(res => cy.log(res));
     cy.visit('http://localhost:3000');
   });
 
   it('user can login', function() {
-    //cy.contains('log in').click(); //?turha
     cy.get('#Password').type('salasana');
     cy.get('#Email').type('random.person@ambientia.fi');
     cy.contains('Log in').click();
