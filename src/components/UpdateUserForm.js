0c26caa57a08179e6c11db2aa17cc647532bf6c6
Simo Savonen <simo.savonen@gmail.com>
2019-08-01 12:14:15 +0300

    activeChallenge pois userDetails sisältä

--- src/components/UpdateUserForm.js
+++ src/components/UpdateUserForm.js
@@ -6,7 +6,6 @@ const UpdateUserForm = props => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [location, setLocation] = useState('Please select one');
-  const [activeChallenge, setActiveChallenge] = useState('');
 
   useEffect(() => {
     if (props.user) {
@@ -16,7 +15,6 @@ const UpdateUserForm = props => {
         setName(result.data.name);
         setEmail(result.data.email);
         setLocation(result.data.location);
-        setActiveChallenge(result.data.activeChallenge);
       });
     }
   }, [props.user]);
@@ -27,8 +25,7 @@ const UpdateUserForm = props => {
       id,
       name,
       email,
-      location,
-      activeChallenge
+      location
     };
     props.updateUser(userDetails);
   };
