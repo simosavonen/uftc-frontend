9661c51fa494e4dda005359c24b1adfac514faca
Simo Savonen <sipesa@utu.fi>
2019-08-02 09:51:06 +0300

    context ympäröi kaiken

--- cypress/integration/login_severaluser_app_spec.js
+++ cypress/integration/login_severaluser_app_spec.js
@@ -4,61 +4,65 @@ describe('UTFC login Users', function() {
       cy.viewport(1280, 720);
       cy.visit('http://localhost:3000');
     });
+
+    it('user random can login', function() {
+      cy.get('#Password').type('salasana');
+      cy.get('#Email').type('random.person@ambientia.fi');
+      cy.contains('Log in').click();
+      cy.contains('log out');
+      cy.contains('log out').click();
+    });
+  
+    it('user sohlo can login', function() {
+      cy.get('#Password').type('salasana');
+      cy.get('#Email').type('sohlo.saheltaja@ambientia.fi');
+      cy.contains('Log in').click();
+      cy.contains('log out');
+      cy.contains('log out').click();
+    });
+  
+    it('user hauska can login', function() {
+      cy.get('#Password').type('salasana');
+      cy.get('#Email').type('hauska.hemmo@ambientia.fi');
+      cy.contains('Log in').click();
+      cy.contains('log out');
+      cy.contains('log out').click();
+    });
+  
+    it('user vakava can login', function() {
+      cy.get('#Password').type('salasana');
+      cy.get('#Email').type('vakava.mielinen-murheilija@ambientia.fi');
+      cy.contains('Log in').click();
+      cy.contains('log out');
+      cy.contains('log out').click();
+    });
+  
+    it('user särmä can login', function() {
+      cy.get('#Password').type('salasana');
+      cy.get('#Email').type('sarma.sarmaaja@ambientia.fi');
+      cy.contains('Log in').click();
+      cy.contains('log out');
+      cy.contains('log out').click();
+    });
+  
+    it('user vaaka can login', function() {
+      cy.get('#Password').type('salasana');
+      cy.get('#Email').type('vaaka.taso@ambientia.fi');
+      cy.contains('Log in').click();
+      cy.contains('log out');
+      cy.contains('log out').click();
+    });
+  
+    it('user nimeton can login', function() {
+      cy.get('#Password').type('salasana');
+      cy.get('#Email').type('nimeton.nimellinen@ambientia.fi');
+      cy.contains('Log in').click();
+      cy.contains('log out');
+      cy.contains('log out').click();
+    });
+
+
   });
 
-  it('user random can login', function() {
-    cy.get('#Password').type('salasana');
-    cy.get('#Email').type('random.person@ambientia.fi');
-    cy.contains('Log in').click();
-    cy.contains('log out');
-    cy.contains('log out').click();
-  });
-
-  it('user sohlo can login', function() {
-    cy.get('#Password').type('salasana');
-    cy.get('#Email').type('sohlo.saheltaja@ambientia.fi');
-    cy.contains('Log in').click();
-    cy.contains('log out');
-    cy.contains('log out').click();
-  });
-
-  it('user hauska can login', function() {
-    cy.get('#Password').type('salasana');
-    cy.get('#Email').type('hauska.hemmo@ambientia.fi');
-    cy.contains('Log in').click();
-    cy.contains('log out');
-    cy.contains('log out').click();
-  });
-
-  it('user vakava can login', function() {
-    cy.get('#Password').type('salasana');
-    cy.get('#Email').type('vakava.mielinen-murheilija@ambientia.fi');
-    cy.contains('Log in').click();
-    cy.contains('log out');
-    cy.contains('log out').click();
-  });
-
-  it('user särmä can login', function() {
-    cy.get('#Password').type('salasana');
-    cy.get('#Email').type('sarma.sarmaaja@ambientia.fi');
-    cy.contains('Log in').click();
-    cy.contains('log out');
-    cy.contains('log out').click();
-  });
-
-  it('user vaaka can login', function() {
-    cy.get('#Password').type('salasana');
-    cy.get('#Email').type('vaaka.taso@ambientia.fi');
-    cy.contains('Log in').click();
-    cy.contains('log out');
-    cy.contains('log out').click();
-  });
-
-  it('user nimeton can login', function() {
-    cy.get('#Password').type('salasana');
-    cy.get('#Email').type('nimeton.nimellinen@ambientia.fi');
-    cy.contains('Log in').click();
-    cy.contains('log out');
-    cy.contains('log out').click();
-  });
+  
 });
