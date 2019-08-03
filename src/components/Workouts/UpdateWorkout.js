59ae7f9c6415b92471b5d9c1ac3931628d0b06ea
Marko Mattila <mmattila75@gmail.com>
2019-08-03 10:35:00 +0300

    changes for code sort for workouts by date

--- src/components/Workouts/UpdateWorkout.js
+++ src/components/Workouts/UpdateWorkout.js
@@ -8,8 +8,18 @@ const UpdateWorkout = props => {
   const [workoutSelected, setWorkoutSelected] = useState(null);
   const [showActivities, setshowActivities] = useState(false);
   const [opened, setOpened] = useState(false);
-  //const [renderKey, setRenderKey] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const actNameTbl = [];
+  let oneTypeAct = [];
+  let oneTypeActLenght;
+
+  if (!props.workouts)
+    return (
+      <p>
+        <b>Have nice day !</b>
+        <p>hmm, ...it seems that workouts was null or undefined</p>
+      </p>
+    );
 
   const handleClick = () => {
     setOpened(!opened);
@@ -21,8 +31,6 @@ const UpdateWorkout = props => {
         setShowModal(false);
       }
     }
-    //    console.log('showActivities after', showActivities);
-    //    console.log('workoutSelected after', workoutSelected);
   };
 
   const Icon = posed.div({
@@ -34,163 +42,138 @@ const UpdateWorkout = props => {
     }
   });
 
-  let oneTypeActLenght;
-
-  const dayAndActivity = () => {
-    const actNameTbl = [];
-    let oneTypeAct = [];
-    //let nameTbl;
-
-    if (!props.workouts) return 'workouts oli null tai undefined';
-
-    //    console.log('day  showActivities', showActivities);
-    //    console.log('day  props workout', props.workouts);
-    //    console.log('day  opened', opened);
-    props.workouts.map(item => {
-      const a = item.activity;
-      const _workoutid = item.id;
-      return item.instances.map(ind => {
-        return actNameTbl.push({
-          date: ind.date,
-          activity: a,
-          workoutid: _workoutid,
-          amount: ind.amount,
-          _id: ind._id
-        });
+  props.workouts.map(item => {
+    const a = item.activity;
+    const _workoutid = item.id;
+    return item.instances.map(ind => {
+      return actNameTbl.push({
+        date: ind.date,
+        activity: a,
+        workoutid: _workoutid,
+        amount: ind.amount,
+        _id: ind._id
       });
     });
+  });
 
-    actNameTbl.sort(cmpAct);
+  actNameTbl.sort(cmpAct);
 
-    function cmpAct(a, b) {
-      if (a.activity < b.activity) {
-        return -1;
-      }
-      if (a.activity > b.activity) {
-        return 1;
-      }
-      return 0;
+  function cmpAct(a, b) {
+    if (a.activity < b.activity) {
+      return -1;
     }
-
-    actNameTbl.map(ind => {
-      if (ind.activity === props.activity.id) {
-        oneTypeAct.push(ind);
-      }
-      return 0;
-    });
-
-    oneTypeActLenght = oneTypeAct.length;
-
-    if (!oneTypeActLenght) {
-      return (
-        <p>
-          <b>Hi there, you can make good exercise!</b>
-        </p>
-      );
+    if (a.activity > b.activity) {
+      return 1;
     }
-    /*
-    const renderKey = () => {
-      return `${new Date().getTime()}`;
-      //return `${0}`;
-    };
-*/
-    const updateCall = () => {
-      // console.log('updatecall r', renderKey());
-      //  console.log('showActivities ', showActivities);
-      if (workoutSelected) {
-        //  console.log('upd w ', workoutSelected);
-        //setRenderKey(!renderKey);
-        //setShowModal(true);
-        return (
-          <div className={`modal ${showModal && 'is-active'}`}>
-            <div className="modal-background" />
-            <div className="modal-content">
-              <UpdateWorkoutForm
-                workout={workoutSelected}
-                updateWorkout={props.updateWorkout}
-                setShowModal={setShowModal}
-              />
-            </div>
-            <button
-              className="modal-close is-large"
-              aria-label="close"
-              onClick={() => {
-                // nameTbl = document.querySelector('.modal');
-                //if (nameTbl) {
-                //  nameTbl.classList.remove('is-active');
-                if (workoutSelected) {
-                  setWorkoutSelected(null);
-                }
-                // }
-                setShowModal(false);
-              }}
-            />
-          </div>
-        );
-      }
-    };
+    return 0;
+  }
 
+  actNameTbl.map(ind => {
+    if (ind.activity === props.activity.id) {
+      oneTypeAct.push(ind);
+    }
+    return 0;
+  });
+
+  oneTypeAct.sort(cmpDate);
+
+  function cmpDate(a, b) {
+    if (a.date < b.date) {
+      return -1;
+    }
+    if (a.date > b.date) {
+      return 1;
+    }
+    return 0;
+  }
+
+  oneTypeActLenght = oneTypeAct.length;
+
+  if (!oneTypeActLenght) {
     return (
-      <>
-        <div
-          className={`columns is-centered is-mobile has-background-dark has-text-white-ter is-size-6-mobile is-size-5-tablet is-size-4-desktop `}
-          style={{ padding: '1vw', margin: '1vw 4vw' }}
-          onClick={handleClick}
-        >
-          <div className="column is-11">{showActivities ? 'Hide activities' : 'Your history'}</div>
-
-          <Icon className="column is-1" pose={opened ? 'up' : 'down'}>
-            <FontAwesomeIcon icon="angle-up" />
-          </Icon>
-        </div>
-
-        {showActivities && (
-          <div>
-            <ul>
-              {oneTypeAct.map(item => (
-                <li
-                  key={item.date + item.amount}
-                  className="is-clickable hover-effect-dark"
-                  onClick={() => {
-                    setWorkoutSelected(item);
-                    setShowModal(true);
-                    // console.log('item', item);
-                  }}
-                >
-                  <span
-                    style={{
-                      fontFamily: 'verdana',
-                      fontSize: 'larger',
-                      fontWeight: '700',
-                      color: '#0f0f0f'
-                    }}
-                  >
-                    {moment(item.date).format('ddd MMM Do')}
-                  </span>
-                  {' \u00b7 '}
-                  <span
-                    style={{
-                      fontFamily: 'verdana',
-                      fontSize: 'larger',
-                      fontWeight: '700',
-                      color: '#ff2457'
-                    }}
-                  >
-                    {item.amount}
-                  </span>
-                </li>
-              ))}
-            </ul>
-            <div>{updateCall()}</div>
-          </div>
-        )}
-      </>
+      <p>
+        <b>Hi there, you can make good exercise!</b>
+      </p>
     );
-  };
+  }
+  //};
 
   return (
     <>
-      <div>{dayAndActivity()}</div>
+      <div
+        className={`columns is-centered is-mobile has-background-dark has-text-white-ter is-size-6-mobile is-size-5-tablet is-size-4-desktop `}
+        style={{ padding: '1vw', margin: '1vw 4vw' }}
+        onClick={handleClick}
+      >
+        <div className="column is-11">{showActivities ? 'Hide activities' : 'Your history'}</div>
+
+        <Icon className="column is-1" pose={opened ? 'up' : 'down'}>
+          <FontAwesomeIcon icon="angle-up" />
+        </Icon>
+      </div>
+
+      {showActivities && (
+        <div>
+          <ul>
+            {oneTypeAct.map(item => (
+              <li
+                key={item.date + item.amount}
+                className="is-clickable hover-effect-dark"
+                onClick={() => {
+                  setWorkoutSelected(item);
+                  setShowModal(true);
+                }}
+              >
+                <span
+                  style={{
+                    fontFamily: 'verdana',
+                    fontSize: 'larger',
+                    fontWeight: '700',
+                    color: '#0f0f0f'
+                  }}
+                >
+                  {moment(item.date).format('ddd MMM Do')}
+                </span>
+                {' \u00b7 '}
+                <span
+                  style={{
+                    fontFamily: 'verdana',
+                    fontSize: 'larger',
+                    fontWeight: '700',
+                    color: '#ff2457'
+                  }}
+                >
+                  {item.amount}
+                </span>
+              </li>
+            ))}
+          </ul>
+          <div>
+            {workoutSelected && (
+              <div className={`modal ${showModal && 'is-active'}`}>
+                <div className="modal-background" />
+                <div className="modal-content">
+                  <UpdateWorkoutForm
+                    workout={workoutSelected}
+                    updateWorkout={props.updateWorkout}
+                    setShowModal={setShowModal}
+                  />
+                </div>
+                <button
+                  className="modal-close is-large"
+                  aria-label="close"
+                  onClick={() => {
+                    if (workoutSelected) {
+                      setWorkoutSelected(null);
+                    }
+                    setShowModal(false);
+                  }}
+                />
+              </div>
+            )}
+          </div>
+        </div>
+      )}
     </>
   );
 };
