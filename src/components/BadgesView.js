a7e728e3e25e70f28bd25f05eaa5134647b46df7
Salomon Honkala <salomon.honkala@gmail.com>
2019-08-05 15:45:47 +0300

    Add a button to show all achievements

--- src/components/BadgesView.js
+++ src/components/BadgesView.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { checkAchievements, badgeRewardsTotal } from '../badges/utils';
+import React, { useState } from 'react';
+import moment from 'moment';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { checkAchievements, badgeRewardsTotal } from '../badges/utils';
 
 const Badge = ({ achievement, activity }) => {
   return (
@@ -20,7 +21,11 @@ const Badge = ({ achievement, activity }) => {
           <p className="content">
             <strong>{achievement.name}</strong>
             <br />
-            Get {achievement.requirement} points from {activity.name}.
+            Get {achievement.requirement} points{' '}
+            {activity
+              ? `from ${activity.name}`
+              : `total on ${moment(achievement.date).format('YYYY-MM-DD')}`}
+            .
             <br />
             <span className="has-text-danger">Reward: {achievement.pointsReward} extra points</span>
           </p>
@@ -31,6 +36,10 @@ const Badge = ({ achievement, activity }) => {
 };
 
 const BadgesView = props => {
+  const [showAll, setShowAll] = useState(false);
+
+  const toggleShowAll = () => setShowAll(!showAll);
+
   const myAchievements = checkAchievements(props.workouts, props.activities, props.achievements);
 
   const badges = myAchievements.map(a => (
@@ -41,6 +50,21 @@ const BadgesView = props => {
     />
   ));
 
+  const allAchievements = props.achievements
+    .filter(a => a.activity)
+    .map(a => (
+      <Badge
+        achievement={a}
+        key={a.id}
+        activity={props.activities.find(ac => ac.id === a.activity)}
+      />
+    ));
+
+  // Daily challenge achievements have a null activity field
+  const dailyChallenges = props.achievements
+    .filter(a => !a.activity)
+    .map(a => <Badge achievement={a} key={a.id} activity={a.activity} />);
+
   return (
     <div>
       <section className="section">
@@ -49,8 +73,29 @@ const BadgesView = props => {
           Your badges
         </h1>
         <h2 className="subtitle">{`Total reward points: ${badgeRewardsTotal(myAchievements)}`}</h2>
+        <div className="section is-size-6-mobile is-size-6-tablet">{badges}</div>
       </section>
-      <div className="section is-size-6-mobile is-size-6-tablet">{badges}</div>
+      <section className="section">
+        <h1 className="title is-4">
+          <FontAwesomeIcon icon="medal" size="2x" />
+          All achievements
+        </h1>
+        <button className="button title is-4" onClick={toggleShowAll}>
+          {showAll ? 'Close' : 'Show all'}
+        </button>
+        {showAll && (
+          <div className="section is-size-6-mobile is-size-6-tablet">{allAchievements}</div>
+        )}
+      </section>
+      {showAll && (
+        <section className="section">
+          <h1 className="title is-4">
+            <FontAwesomeIcon icon="medal" size="2x" />
+            Daily challenges
+          </h1>
+          <div className="section is-size-6-mobile is-size-6-tablet">{dailyChallenges}</div>
+        </section>
+      )}
     </div>
   );
 };
