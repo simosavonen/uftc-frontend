1c78aab11ae45bc095bf3613ee7c6a18476b8bfa
Simo Savonen <simo.savonen@gmail.com>
2019-08-09 19:13:31 +0300

    show how many points one unit of workouts would give

--- src/components/Activities/ActivityDetails.js
+++ src/components/Activities/ActivityDetails.js
@@ -13,11 +13,14 @@ const ActivityDetails = ({ activity }) => {
         <figure className="media-left image is-48x48">{icon(activity.icon)}</figure>
         <div className="media-content">
           <div className="content">
-            <p className="title is-4">{activity.name}</p>
+            <p className="title is-5">{activity.name}</p>
             <p className="subtitle is-6">{activity.description}</p>
           </div>
         </div>
-        <div className="media-right" />
+        <div className="media-right has-text-right">
+          <h1 className="title is-5">{activity.points} points</h1>
+          <h2 className="subtitle is-6">{activity.unit}</h2>
+        </div>
       </article>
     </Link>
   );
