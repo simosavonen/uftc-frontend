fa9d8fec6dd3fd6b49f8af131de31fff9e1e5fd2
Simo Savonen <simo.savonen@gmail.com>
2019-08-19 13:06:49 +0300

    better way to filter out NaN input, do nothing if user accidentally enters text

--- src/components/Workouts/UpdateWorkoutForm.js
+++ src/components/Workouts/UpdateWorkoutForm.js
@@ -28,14 +28,13 @@ const UpdateWorkoutForm = props => {
   };
 
   const handleAmountChange = event => {
-    let theValue = Number(event.target.value);
-    if (Number.isNaN(theValue)) {
-      theValue = 1;
+    if (!Number.isNaN(+event.target.value)) {
+      let theValue = +event.target.value;
+      if (theValue < 0) {
+        theValue = Math.abs(theValue);
+      }
+      setAmount(theValue);
     }
-    if (theValue < 0) {
-      theValue = Math.abs(theValue);
-    }
-    setAmount(theValue);
   };
 
   const dontAllowZero = event => {
